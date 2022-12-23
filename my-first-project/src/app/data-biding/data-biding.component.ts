86c60f8314766dbeefa5ab825ba4c09d4a4c6a20
marjoriegaioso <marjoriegaioso99@gmail.com>
2022-12-23 12:44:09 -0300

    23.12

--- my-first-project/src/app/data-biding/data-biding.component.ts
+++ my-first-project/src/app/data-biding/data-biding.component.ts
@@ -12,10 +12,16 @@ export class DataBidingComponent implements OnInit{
   public maisUm: number = 1;
 
   public botaoDesligado: boolean = true; 
-  public imgSrc: string = "https://i.pinimg.com/originals/10/fc/86/10fc869db0963aa2cb3631fb05288f26.jpg";
-
-constructor (){ }
-ngOnInit(): void {
+  public imgSrc: string = "https://i.pinimg.com/originals/10/fc/86/10fc869db0963aa2cb3631fb05288f26.jpg"
   
-}
+  public position: {x: number, y: number} = {x: 0, y: 0};
+  constructor (){ }
+  ngOnInit(): void { }
+  public alertaInfo (valor: any){
+    console.log(valor); }
+
+  public mouseMoveTeste(valor: any) {
+    console.log(valor);
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;}
 }
