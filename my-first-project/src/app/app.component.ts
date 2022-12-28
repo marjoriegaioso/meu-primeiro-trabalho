a9d64c2192840063216f38d92c63f5050c031e1c
marjoriegaioso <marjoriegaioso99@gmail.com>
2022-12-28 19:53:16 -0300

    28.12

--- my-first-project/src/app/app.component.ts
+++ my-first-project/src/app/app.component.ts
@@ -3,12 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-data-biding></app-data-biding>
+  <!--
+    <app-title *ngIf="destruir"></app-tittle>
+    <br>
+    <button (click)="destruirComponent()">Destruir componente</button>
+  --> 
   <!-- app-diretivas-estruturais></app-diretivas-estruturais> -->
+  <!--
+   <app-diretivas-atributos>
+    <h1> Aula de Diretivas Atributos</h1>
+    <h3> Aula final </h3>
+  </app-diretivas-atributos>
+  
   <app-diretivas-atributos>
+    <h1> Marjorie </h1>
+    <h3> Bird </h3>
+  </app-diretivas-atributos>
   <router-outlet></router-outlet>
+  -->
+  <app-diretivas-atributos></app-diretivas-atributos>
   `
- 
 })
 export class AppComponent implements OnInit { 
   
--- my-first-project/src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ my-first-project/src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -1,3 +1,4 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./diretivas-atributos.component.scss']
 })
 export class DiretivasAtributosComponent {
+  public valor: boolean= true; 
+  public heightPx: string = "20px";
+  public backgroundColor: string = 'red';
 
-}
+  public nome: string = "";
+  public list: Array<{nome: string}> = [{nome: "Marjorie"}];
+  public date = new Date ();
+
+  constructor(){}
+  
+  ngOnInit(): void {
+    setInterval ( ()=>{
+      if(this.valor){
+        this.valor=false;
+      }else {
+        this.valor=true;
+      }
+      if(this.heightPx=="20px"){
+        this.heightPx = "50px";
+        this.backgroundColor = "blue";
+      }else {
+        this.heightPx = "20px";
+        this.backgroundColor= "red";
+      }
+    },2000);
+   }
+  public salvar (){
+    this.list.push({nome:this.nome});
+    this.nome = "";
+  }
+  }
